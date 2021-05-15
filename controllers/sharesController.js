a08f645b8ee4b79d41b2e7f4050a9d61d64c7a4c
dogubo mamuzo joshua <51678953+c0deZ0ne@users.noreply.github.com>
2021-05-15 01:08:27 +0100

    SELL

--- controllers/sharesController.js
+++ controllers/sharesController.js
@@ -5,10 +5,9 @@ const urlencoded = bodyParser.urlencoded({ extended: false });
 const UserShares = require('../models/UserShares');
 const connection = require('../config/dbconnection');//getting to connect to perform action
 const axios = require('../config/axiosConfig')
- const finnhubClient = require('../config/finhubConfig');
+const finnhubClient = require('../config/finhubConfig');
 const Messages = require('../config/activityStatus/MessageGen');
 const User = connection.models.User//all seaches access user db model and find matches
-
 
 module.exports = async function router(app) {
     // this contols routh and passing data to view engine
@@ -23,23 +22,23 @@ module.exports = async function router(app) {
 
             } else {
                 let before = result.Shares.length;
-                
+
                 let current_ballance = result.acountballance;
 
-                if(!current_ballance>0){
-                    return(res.status(200).json(new Messages(201,'account ballance is too low')))
+                if (!current_ballance > 0) {
+                    return (res.status(200).json(new Messages(201, 'account ballance is too low')))
 
-                }else{
-                    let share_cost = Rate*volume_bought
-                    if(current_ballance<share_cost){
-                        return  (res.status(200).json(new Messages(201,'account ballance is too low')));
+                } else {
+                    let share_cost = Rate * volume_bought
+                    if (current_ballance < share_cost) {
+                        return (res.status(200).json(new Messages(201, 'account ballance is too low')));
                     }
                 }
                 result.Shares.push(share)
                 result.save().then((result) => {
                     let after = result.Shares.length
                     if (after > before) {
-                        
+
                         let cost = Rate * volume_bought;
                         let accountBallance = current_ballance - cost
                         result.acountballance = accountBallance,
@@ -66,129 +65,72 @@ module.exports = async function router(app) {
             console.log('there was exeption')
         )
 
-    }).get('/quote/:symbole',isAuth,(req,res,next)=>{
-    
-        let symbol= req.params.symbole;
+    }).get('/quote/:symbole', isAuth, (req, res, next) => {
+
+        let symbol = req.params.symbole;
         let user = req.session.passport.user;
-       symbol= symbol.toUpperCase();
-       
-          finnhubClient.quote(`${symbol}`, (error, data, response) => {    
-              let lastupdate = Date.now()
-            User.findById(user).then((result)=>{
-                let copyResult  = result.Shares;
+        symbol = symbol.toUpperCase();
+
+        finnhubClient.quote(`${symbol}`, (error, data, response) => {
+            let lastupdate = Date.now()
+            User.findById(user).then((result) => {
+                let copyResult = result.Shares;
                 // filter shares to be updated
-                let filtershares = copyResult.filter((share)=>share.symbole==`${symbol}`);
-                (!filtershares.length>0)?res.status(200).json(new Messages(200,`${symbol} is not in your shares list`)):
-                //console.log(filtershares);
-               filtershares.map((share)=>{
-                    if(share.symbole == `${symbol}`){
-                        const {symbole,CompanyNmae,_id} = share;
-                       result.Shares.map((share)=>{ if(share._id == _id){return share.Price_update.push(data)}});
-                       result.stuckTracker.push({_id,lastupdate:lastupdate,symbole,CompanyNmae,data});
-                           result.save().then((doc)=>{res.status(201).json({messages:new Messages(201,'stuckupdated success'),data})})
-                        
-                    }else{
-                        return
-                    }
-                }); 
+                let filtershares = copyResult.filter((share) => share.symbole == `${symbol}`);
+                (!filtershares.length > 0) ? res.status(200).json(new Messages(200, `${symbol} is not in your shares list`)) :
+                    //console.log(filtershares);
+                    filtershares.map((share) => {
+                        if (share.symbole == `${symbol}`) {
+                            const { symbole, CompanyNmae, _id } = share;
+                            result.Shares.map((share) => { if (share._id == _id) { return share.Price_update.push(data) } });
+                            result.stuckTracker.push({ _id, lastupdate: lastupdate, symbole, CompanyNmae, data });
+                            result.save().then((doc) => { res.status(201).json({ messages: new Messages(201, 'stuckupdated success'), data }) })
+
+                        } else {
+                            return
+                        }
+                    });
             })
         })
-      
-    
-   
 
-
-       
-
-          }).post("/sellshares/:id",isAuth,(req,res,next)=>{
-
-            //get shares update with axios
-
-            
-        
+    }).post("/sellshares/:id", isAuth, (req, res, next) => {
+        //get shares update with axios
         let user = req.session.passport.user
         let shareId = req.params.id
-        User.findById(user).then((result)=>{
-            soldShare = result.Shares.filter((share)=>{
+        User.findById(user).then((result) => {
+            soldShare = result.Shares.filter((share) => {
                 return (share._id == `${shareId}`)//getting share with id and amout worth
             })
-            if (!result.Shares.length>0) {
-                return res.status(200).json({message:new Messages(204,'no shares to sell')})
-            }else if(!soldShare.length>0){
-               return  res.status(200).json({message:new Messages(204,'No succh share available '),availableShares:result.Shares})
+            if (!result.Shares.length > 0) {
+                return res.status(200).json({ message: new Messages(204, 'no shares to sell') })
+            } else if (!soldShare.length > 0) {
+                return res.status(200).json({ message: new Messages(204, 'No succh share available '), availableShares: result.Shares })
             }
-            !result?res.status(200).json({message:new Messages(204,' no user')}):
-            soldShare.map((soldShare)=>{
-                const {Rate,volume_bought,_id,symbole} = soldShare;
-                let current_data = async () => await axios.get(`/quote/:${symbole}`);
-                console.log(current_data());
-                //res.send('in progress',current_data);
-                // let price = Rate*volume_bought;
-                // result.acountballance+=price;
-                // let userShares =  result.Shares.filter((share)=>{
-                //     return (share._id != `${shareId}`)//removing share with id
-                // })
-                // result.Shares = userShares;//updating shares
-                // result.save().then(()=>{res.status(200).json({message:new Messages(200,'share sold success'),price,Shares:userShares})})
-            })
-          
+            !result ? res.status(200).json({ message: new Messages(204, ' no user') }) :
+                soldShare.map((soldShare) => {
+                    const { Rate, volume_bought, _id, symbole } = soldShare;
 
-           
-            
+                    finnhubClient.quote(`${symbole}`, (error, data, response) => {
+                        if (error) {
+                            return res.status(200).json({ message: new Messages(200, "error updating current price") })
+                        } else {
 
+                            res.status(200).json(new Messages(200,"in progress"), data);
+                            let price = data.pc * volume_bought;
+                            console.log(data.pc)
+                            result.acountballance += price;
+                            let userShares = result.Shares.filter((share) => {
+                                return (share._id != `${shareId}`)//removing share with id
+                            })
+                            result.Shares = userShares;//updating shares
+                            result.save().then(() => { res.status(200).json({ message: new Messages(200, 'share sold success'), price, Shares: userShares }) })
+                        }
+
+                    })
+                })
 
         }).catch(
             console.log('thre was error')
         )
-        //console.log(req.params)
-    })
-
-
-
-    
-    //:::::::::::::::::::todo edit routh:::::::::::::
-
-    app.post('/edit', isAuth, urlencoded, (req, res, next) => {
-        const { date_crerated, time, expected_endDate, id, item, index } = req.body;
-
-        let user = req.session.passport.user;
-        let initial_value = req.user.todoes[index].item
-        console.log(initial_value);
-        User.findById(user).then((result) => {
-            result.todoes[index].item = item;
-            result.save().then((result) => {
-                let after_value = result.todoes[index].item
-                if (initial_value != after_value) {
-                    res.message = new Messages(200, "Update was success :)");
-                    res.status(200).json(res.message);
-                    console.log('change success');
-
-                } else {
-                    res.message = new Messages(400, "there was error Updaying:(");
-                    return res.status(400).json(result.message), next(null, res.message);
-                }
-            })
-        })
-    });
-
-    // deleting todoes using filter method:::::////
-
-    app.post('/delete', isAuth, urlencoded, (req, res, next) => {
-        console.log(req.body);
-        const index = req.body.index;
-        let user = req.session.passport.user;
-        let initial_value = req.user.todoes.length;
-        User.findById(user).then((result) => {
-            if (!result) {
-                res.message = new Messages(401, " You are not authorised :( ")
-                return res.status(401).json(res.message), next(null, res.message);
-            } else {
-                result.todoes[index].remove();
-                res.message = new Messages(200, " success :) ")
-                result.save().then(() => {
-                    return res.status(200).json(res.message), next(null, res.message);
-                })
-            }
-        })
     })
 }
